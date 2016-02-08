853cfb5a0db1c65c0aa9c41e7f2224939b61272a
myrka <myroslava2337@mail.ru>
2016-02-08 20:56:43 +0000

    Login page improvements

--- app/pages/login/Login.jsx
+++ app/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import LinkedStateMixin from 'react-addons-linked-state-mixin';
+import fetch from 'isomorphic-fetch';
 
 import './login.scss';
 
@@ -7,7 +8,36 @@ import './login.scss';
 class Login extends React.Component {
     state = {
         nickname: '',
-        password: ''
+        password: '',
+        warningMessage: ''
+    };
+
+    _sendUserData = () => {
+        const { nickname, password } = this.state;
+
+        if (nickname.length === 0 || password.length === 0) {
+            this.setState({
+                warningMessage: "* all fields must be non-empty!"
+            });
+        } else {
+            fetch('/api/login', {
+                method: 'post',
+                body: JSON.stringify({
+                    nickname, // nickname: nickname
+                    password
+                })
+            }).then((res) => {
+                console.log('result', res);
+            }).catch((ex) => {
+                console.log('error', ex);
+            });
+        }
+    };
+
+    _clearWarning = () => {
+        this.setState({
+            warningMessage: ""
+        });
     };
 
     render() {
@@ -20,8 +50,26 @@ class Login extends React.Component {
                         <input
                                 id="nickname"
                                 type="text"
+                                onFocus={this._clearWarning}
                                 valueLink={this.linkState('nickname')}/>
                     </section>
+                    <section className="input-line">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                                id="password"
+                                type="text"
+                                onFocus={this._clearWarning}
+                                valueLink={this.linkState('password')}/>
+                    </section>
+                    {
+                        this.state.warningMessage.length !== 0 &&
+                            <div className="warning">{this.state.warningMessage}</div>
+                    }
+                    <button
+                            className="login-button"
+                            onClick={this._sendUserData}>
+                        Log in
+                    </button>
                 </section>
             </section>
         );
