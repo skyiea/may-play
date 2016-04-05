0a15f021f2abe96bcb87d04e841d8dee62128037
Sergiy Murygin <skyemurygin@gmail.com>
2016-04-05 22:43:56 +0300

    [Button] Improvements

--- frontend/components/button/Button.jsx
+++ frontend/components/button/Button.jsx
@@ -1,8 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import classnames from 'classnames';
+import CSSModules from 'react-css-modules';
 import { ReactClass } from 'react-core-decorators';
 import omit from 'lodash.omit';
 
+import styles from './Button.scss';
+
+@CSSModules(styles, { allowMultiple: true })
 @ReactClass
 class Button extends Component {
     static propTypes = {
@@ -11,10 +16,13 @@ class Button extends Component {
             PropTypes.number,
             PropTypes.string
         ]),
-        onClick: PropTypes.func,
         blurOnEscape: PropTypes.bool,
-        blurOnEnter: PropTypes.bool,
-        blurOnClick: PropTypes.bool
+        blurOnEnter : PropTypes.bool,
+        blurOnClick : PropTypes.bool,
+
+        onMouseDown : PropTypes.func,
+        onClick     : PropTypes.func,
+        onKeyDown   : PropTypes.func
     };
 
     static defaultProps = {
@@ -24,6 +32,10 @@ class Button extends Component {
         blurOnClick: false
     };
 
+    _isFocused() {
+        return document.activeElement === ReactDOM.findDOMNode(this);
+    }
+
     _blur() {
         ReactDOM.findDOMNode(this).blur();
     }
@@ -31,6 +43,7 @@ class Button extends Component {
     _handleKeyDown = (e) => {
         const {
             onClick,
+            onKeyDown,
             blurOnEscape,
             blurOnEnter
         } = this.props;
@@ -39,11 +52,29 @@ class Button extends Component {
         const ESCAPE_CODE = 27;
 
         if (e.keyCode === ENTER_CODE) {
-            onClick(e);
+            onClick && onClick(e);
             blurOnEnter && this._blur();
         } else if (e.keyCode === ESCAPE_CODE && blurOnEscape) {
             this._blur();
         }
+
+        onKeyDown && onKeyDown(e);
+    };
+
+    _handleMouseDown = (e) => {
+        const {
+            onMouseDown
+        } = this.props;
+
+        // this is necessary because even if tabIndex is -1 it is still possible to focus button via pointer
+        e.preventDefault();
+
+        // still need to blur from previously focused element
+        if (document.activeElement) {
+            document.activeElement.blur();
+        }
+
+        onMouseDown && onMouseDown(e);
     };
 
     _handleClick = (e) => {
@@ -52,7 +83,7 @@ class Button extends Component {
             blurOnClick
         } = this.props;
 
-        onClick(e);
+        onClick && onClick(e);
 
         if (blurOnClick) {
             this._blur();
@@ -60,7 +91,7 @@ class Button extends Component {
     };
 
     componentDidUpdate(prevProps) {
-        if (!prevProps.disabled && this.props.disabled && document.activeElement === ReactDOM.findDOMNode(this)) {
+        if (!prevProps.disabled && this.props.disabled && this._isFocused()) {
             this._blur();
         }
     }
@@ -74,15 +105,22 @@ class Button extends Component {
         } = this.props;
 
         const filteredProps = omit(otherProps, [
+            'styles',
             'onClick',
+            'onKeyDown',
+            'onMouseDown',
             'blurOnEscape',
             'blurOnEnter',
             'blurOnClick'
         ]);
+        
+        const styleNames = classnames('button', { disabled });
 
         return (
             <div {...filteredProps}
+                    styleName={styleNames}
                     tabIndex={disabled ? null : tabIndex}
+                    onMouseDown={this._handleMouseDown}
                     onClick={disabled ? null : this._handleClick}
                     onKeyDown={this._handleKeyDown}>
                 {children}
